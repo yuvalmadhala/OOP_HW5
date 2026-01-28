3fa8ddb3144fa0d19f8a58cefd894215cdee2adb
yuval <yuvalmadhala@campus.technion.ac.il>
2026-01-29 00:00:09 +0200

    Rushhour

--- part2/RushHour.h
+++ part2/RushHour.h
@@ -1,3 +1,28 @@
 #ifndef PART2_RUSHHOUR_H
 #define PART2_RUSHHOUR_H
+#include "Board.h"
+#include "Move.h"
+#include "List.h"
+
+/*
+Strctures:
+
+1. checkWin:
+    - template parameters:
+        - BoardType: the board to check
+    - return value:
+        - true if red car is one move away from exit, false otherwise
+    -fields:
+        - result : bool
+
+
+2. checkSolution
+*/
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ CheckWin ~~~~~~~~~~~~~~~~~~~~~~*/
+template<typename BoardType>
+struct CheckWin {
+    static 
+};
+
+
 #endif //PART2_RUSHHOUR_H
